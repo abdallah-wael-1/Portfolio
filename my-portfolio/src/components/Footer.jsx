fc732c99b67f7ef2a133cbb66689ec1ae2e8e9d5
Abdallah Wael El Saadany <abdallahelsadany18@gmail.com>
2025-11-24 01:54:52 +0200

    Update Footer.jsx

--- my-portfolio/src/components/Footer.jsx
+++ my-portfolio/src/components/Footer.jsx
@@ -1,4 +1,3 @@
-// src/components/Footer.jsx
 import React from "react";
 import { Box, Typography, Link, IconButton } from "@mui/material";
 import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
